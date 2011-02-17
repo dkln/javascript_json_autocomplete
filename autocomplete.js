febc7cb1e7e571de5161591a62051e1e383d17d7
altovista <webmaster@altovista.nl>
2011-02-16 23:14:01 -0800

    Fixed some bugs + added new feature for rendering optional extra fields besides the default name attribute

--- autocomplete.js
+++ autocomplete.js
@@ -1,7 +1,4 @@
-/**
- * JSON Javascript autocomplete class
- * @author D Lawson <webmaster@altovista.nl>
- */
+// Super 1337 Autocompletion class
 var Autocomplete = function(root) {
   this.root = $(root);
   this.initialize();
@@ -12,7 +9,7 @@ Autocomplete.prototype = {
   initialize: function() {
     this.selectedValue = this.root.val();
 
-   if(this.root.attr('tagName') == 'INPUT' && this.root.attr('type') == 'text') {
+    if(this.root.attr('tagName') == 'INPUT' && this.root.attr('type') == 'text') {
       this.input = this.root;
       this.freeInput = true;
 
@@ -21,12 +18,14 @@ Autocomplete.prototype = {
       this.freeInput = false;
       this.input = this.createInput();
       this.input.attr("autocomplete", "off");
+
       // remove from DOM
       this.root.remove();
+
       // multiselect?
       this.multiselect = this.root.attr('multiple');
-      // add a list with multi vals
 
+      // add a list with multi vals
       if(this.multiselect) {
         // add a div
         this.multiselectListContainer = this.createMultiselectListContainer();
@@ -42,17 +41,20 @@ Autocomplete.prototype = {
     // create dropdown and hidden fields
     this.dropdown = this.createDropdown();
 
-    if(!this.multiselect) {
+    if(!this.multiselect && !this.freeInput) {
       this.hiddenField = this.createHiddenField();
       this.hiddenField.val(this.selectedValue);
     }
 
     this.autocompleteUrl = this.root.attr('autocomplete_url');
 
+    this.input[0].autocomplete = this;
+
     // add keypress listener
     this.input.keypress(this.bind(this.handleKeypress));
     this.input.click(this.bind(this.handleInputClick));
     this.input.blur(this.bind(this.handleBlur));
+
     // finally, check the multivalues and autofill them
     if(this.multiselect) {
       this.updateMultiVals();
@@ -66,8 +68,10 @@ Autocomplete.prototype = {
 
     container.attr('id', this.root.attr('id') + '_value_list_container');
     container.attr('className', 'autocomplete_value_list');
+
     // insert dropdown after textfield
     this.input.after(container);
+
     return container;
   },
 
@@ -77,6 +81,7 @@ Autocomplete.prototype = {
 
     list.attr('id', this.root.attr('id') + '_value_list');
     this.multiselectListContainer.append(list);
+
     return list;
   },
 
@@ -86,12 +91,13 @@ Autocomplete.prototype = {
     var vals = this.selectedValue;
 
     if(!vals) vals = [];
+
     // loop every selected val
     for(i = 0; i < vals.length; i++) {
       // do already have a list item with this ID?
       if(this.multiselectList.find('li[rel="' + vals[i] + '"]').length == 0) {
         // no, so go request it!
-        $.getJSON(this.itemUrl.replace('%25', vals[i]), this.bind(this.handleUpdateMultiValsResponse));
+        $.getJSON(this.itemUrl.replace('%', vals[i]).replace('%25', vals[i]), this.bind(this.handleUpdateMultiValsResponse));
       }
     }
   },
@@ -101,15 +107,29 @@ Autocomplete.prototype = {
   handleUpdateMultiValsResponse: function(data) {
     // we have received one item, good!
     var item;
+    var i = 0;
+    var list;
 
     // valid data?
     if(data && this.multiselectList.find('li[rel="' + data.id + '"]').length == 0) {
       // yes!
       // add this to the unsorted list
       item = $('<li></li>');
-      item.text(data.name);
+      item.append('<h3>' + data.name + '</h3>');
+
+      if(data.extra && data.extra.length > 0) {
+        table = $('<table></table>');
+
+        for(i = 0; i < data.extra.length; i++) {
+          table.append('<tr><th>' + data.extra[i][0] + '</th><td>' + data.extra[i][1] + '</td></tr>');
+        }
+
+        item.append(table);
+      }
+
       item.attr('rel', data.id);
       item.click(this.bind(this.handleMultiValItemClick));
+
       // add to list
       this.multiselectList.append(item);
     }
@@ -121,19 +141,21 @@ Autocomplete.prototype = {
     var hidden;
 
     if(!values) values = [];
+
     for(i = 0; i < values.length; i++) {
       if($('input#' + this.root.attr('id') + '_val_' + values[i]).length == 0) {
         hidden = $('<input type="hidden" />');
         hidden.attr('id', this.root.attr('id') + '_val_' + values[i]);
         hidden.attr('name', this.root.attr('name'));
         hidden.val(values[i]);
+
         this.input.after(hidden);
       }
     }
   },
 
   // catches the click event on a multival item
-  handleMultiValItemClick: function() {
+  handleMultiValItemClick: function(event) {
     // remove from values
     var item = $(event.currentTarget);
     var values = [];
@@ -143,19 +165,24 @@ Autocomplete.prototype = {
 
     // remove from values
     values = this.selectedValue;
+
     for(i = 0; i < values.length; i++) {
       if(values[i] != item.attr('rel'))
         newValues.push(values[i]);
     }
+
     // set new array
     this.selectedValue = newValues;
     this.root.val(this.selectedValue);
+
     // also remove hidden field!
     hidden = $('input#' + this.root.attr('id') + '_val_' + item.attr('rel'));
+
     if(hidden) {
       hidden.remove();
       hidden = null;
     }
+
     item.remove();
     item = null;
   },
@@ -170,16 +197,25 @@ Autocomplete.prototype = {
     if(this.root.attr('tagName') == 'SELECT') {
       if(this.multiselect) {
         // something...
+
       } else {
         this.input.val(this.currentSelectboxText());
+
       }
     }
+  },
+
+  // bind this obj
+  bind: function(funct) {
+    var context = this;
+    return function() {
+      return funct.apply(context, arguments);
+    };
   },
 
   // returns the current selected text in a <select> box
   currentSelectboxText: function() {
     var option = this.root.find('option[value="' + this.selectedValue + '"]');
-
     return option.text();
   },
 
@@ -192,6 +228,7 @@ Autocomplete.prototype = {
     }
     else if(event.keyCode == 27) {
       this.dropdown.hide();
+
     } else {
       window.clearTimeout(this.timer);
       this.timer = window.setTimeout(this.handleKeypressTimer, 300, this);
@@ -217,6 +254,7 @@ Autocomplete.prototype = {
   // displays the dropdown
   showDropdown: function() {
     window.clearTimeout(this.blurTimer);
+
     this.repositionDropdown();
     this.dropdown.show();
     this.dropdown.css('width', this.input.innerWidth());
@@ -224,36 +262,47 @@ Autocomplete.prototype = {
 
   // filters autocompletion
   filter: function() {
-    $.getJSON(this.autocompleteUrl.replace('%25', this.input.val()), this.bind(this.handleFilterResponse));
+    this.input.parent().addClass('loading');
+    $.getJSON(this.autocompleteUrl.replace('%25', this.input.val()).replace('%', this.input.val()), this.bind(this.handleFilterResponse));
   },
 
   // catches response of ajax filter
   handleFilterResponse: function(data) {
     this.dropdown.empty();
-    hideLoader();
+    this.input.parent().removeClass('loading');
+
     // only one result?
     if(!data || data.length == 0) {
+      this.input.parent().addClass('not-found')
       this.dropdown.hide();
+
     } else {
       // build the list!
       var i = 0;
       var list = $('<ul></ul>');
       var item;
       var link;
+
+      this.input.parent().removeClass('not-found')
+
       // add list to dropdown
       this.dropdown.append(list);
+
       // loop every json result
       for(i = 0; i < data.length; i++) {
         // add to list
         item = $('<li></li>');
         item.attr('rel', data[i].id);
         item.text(data[i].name);
+
         // add clikc
         item.click(this.bind(this.handleListItemClick));
+
         list.append(item);
       }
+
       // only one possibility?
-      if(data.length == 1 && data[0].name != this.input.val()) {
+      if(data.length == 1 && data[0].name != this.input.val() && this.root.attr('autoselect')) {
         this.dropdown.hide();
         window.clearTimeout(this.timer);
         this.timer = window.setTimeout(this.bind(this.handleOnlyOneRowTimer), 300, this);
@@ -277,8 +326,10 @@ Autocomplete.prototype = {
     this.dropdown.hide();
 
     if(this.multiselect) {
-      var vals = this.selectedValue;
+      this.input.parent().removeClass('not-found');
+      this.input.parent().addClass('found');
 
+      var vals = this.selectedValue;
       if(!vals) vals = [];
 
       vals.push(item.attr('rel'));
@@ -290,6 +341,7 @@ Autocomplete.prototype = {
       this.selectedValue = item.text();
 
     } else {
+
       this.selectedValue = item.attr('rel');
       this.hiddenField.val(item.attr('rel'));
       this.input.val(item.text());
@@ -297,6 +349,7 @@ Autocomplete.prototype = {
 
     this.root.val(this.selectedValue);
     this.input.focus();
+
     // auto submit?
     if(this.root.attr('autosubmit') == 'autosubmit') {
       // submits automaticly the form where the element is placed in
@@ -322,11 +375,15 @@ Autocomplete.prototype = {
   // creates a dropdown-box and positions it relative to the root element
   createDropdown: function() {
     var dd = $('<div></div>');
+
     dd.attr('id', this.root.attr('id') + '_dropdown');
     dd.attr('className', 'autocomplete_dropdown');
+
     dd.hide();
+
     // insert dropdown after textfield
     this.input.after(dd);
+
     return dd;
   },
 
@@ -334,11 +391,12 @@ Autocomplete.prototype = {
   createHiddenField: function() {
     // create a new root
     var hidden = $('<input type="hidden" />');
-
     hidden.attr('id', this.root.attr('id'));
     hidden.attr('name', this.root.attr('name'));
+
     // insert after textfield
     this.input.after(hidden);
+
     return hidden;
   },
 
@@ -346,22 +404,22 @@ Autocomplete.prototype = {
   createInput: function() {
     var input = $('<input type="text" />');
     var id = this.root.attr('id') + '_input';
-
     input.attr('id', id);
     input.attr('name', id);
     input.attr('className', 'autocomplete_input');
+    input.attr('placeholder', this.root.attr('placeholder'));
+
     // insert after root
     this.root.after(input);
 
     return input;
-  },
-
-  // bind this obj
-  bind: function(funct) {
-    var context = this;
-
-    return function() {
-      return funct.apply(context, arguments);
-    };
   }
+}
+
+function applyAutocomplete() {
+  $('.autocomplete').each(function() {
+    if(!this.autocomplete) {
+      this.autocomplete = new Autocomplete(this);
+    }
+  });
 }
